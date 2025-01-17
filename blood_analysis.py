36bf31bfb6a85e2307abb8cb5b5d6320467ca868
David Ward <david.a.ward@duke.edu>
2025-01-17 12:12:25 -0500

    Wrote simple text interface for blood analysis

---
+++ blood_analysis.py
@@ -0,0 +1,11 @@
+def interface():
+    print("Blood Analysis Menu")
+    while True:
+        print("Options:")
+        print("9 - Quit")
+        choice = input("Enter test to analyze: ")
+        if choice == "9":
+            break
+    print("Exiting...")
+        
+interface()
