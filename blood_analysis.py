fa3d463fb5419ae95447aa27072a82c9264a5cc6
David Ward <david.a.ward@duke.edu>
2025-01-17 12:30:28 -0500

    Program classifies HDL result and outputs to screen

--- blood_analysis.py
+++ blood_analysis.py
@@ -14,10 +14,26 @@ def interface():
 def hdl_analysis():
     print("HDL")
     hdl_result = get_hdl_test_result()
+    hdl_class = analyze_hdl_result(hdl_result)
+    output_hdl_result(hdl_result, hdl_class)
+    
     
 def get_hdl_test_result():
     hdl_value = input("Enter the HDL test result: ")
     hdl_value = int(hdl_value)
     return hdl_value
+        
+def analyze_hdl_result(hdl_value):
+    if hdl_value >= 60:
+        return "Normal"
+    elif 40 <= hdl_value < 60:
+        return "Borderline Low"
+    else:
+        return "Low"
+        
+def output_hdl_result(hdl_result, hdl_class):
+    print("For an HDL value of {}, the result is {}".format(hdl_result,
+                                                            hdl_class))
+                                                            
         
 interface()
