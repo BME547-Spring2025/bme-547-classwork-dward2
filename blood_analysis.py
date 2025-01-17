914440818f59e021400c727a6f2147a0759023ba
David Ward <david.a.ward@duke.edu>
2025-01-17 12:19:36 -0500

    Added HDL test option to main interface menu

--- blood_analysis.py
+++ blood_analysis.py
@@ -2,10 +2,16 @@ def interface():
     print("Blood Analysis Menu")
     while True:
         print("Options:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter test to analyze: ")
         if choice == "9":
             break
+        elif choice == "1":
+            hdl_analysis()
     print("Exiting...")
+    
+def hdl_analysis():
+    print("HDL")
         
 interface()
