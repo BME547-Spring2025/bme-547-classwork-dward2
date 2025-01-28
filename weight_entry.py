c26f9dd2b32470bd3bf98fb9d158feea49cf1ef7
David Ward <david.a.ward@duke.edu>
2025-01-28 14:39:25 -0500

    Modified weight_entry code to work with wider range of entries

--- weight_entry.py
+++ weight_entry.py
@@ -7,12 +7,17 @@ def input_weight_entry():
 
 
 def parse_weight_input(weight_input):
-    weight, units = weight_input.split(' ')
-    weight = int(weight)
-    if units == "lb":
-        weight_kg = convert_lb_to_kg(weight)
+    if weight_input.strip(" ").find(" ") < 0:
+        weight, units = parse_when_no_space(weight_input)
     else:
+        weight, units = weight_input.split()
+    weight = float(weight)
+    if units.lower() in ["lb", "lbs", "pound", "pounds"]:
+        weight_kg = convert_lb_to_kg(weight)
+    elif units.lower() in ["kg", "kgs", "kilos", "kilograms"]:
         weight_kg = weight
+    else:
+        weight_kg = 0
     weight_kg = round(weight_kg)
     return weight_kg
 
@@ -20,6 +25,18 @@ def parse_weight_input(weight_input):
 def convert_lb_to_kg(weight_lb):
     weight_kg = weight_lb / 2.20462
     return weight_kg
+    
+
+def parse_when_no_space(weight_input):
+    weight_in = weight_input.strip(" ")
+    for i, char in enumerate(weight_in):
+        if char.isnumeric() or char in ".-":
+            pass
+        else:
+            break
+    weight = weight_input[:i]
+    units = weight_input[i:]
+    return weight, units
 
 
 if __name__ == "__main__":
