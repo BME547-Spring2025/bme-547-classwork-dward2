36ecde4ee44fce0fe50ddb828ea3aa673845c1cb
David Ward <david.a.ward@duke.edu>
2025-02-05 16:40:47 -0500

    Modify unit tests for use of dictionaries in database

--- test_database.py
+++ test_database.py
@@ -1,17 +1,21 @@
-from database import db
-
-
 def test_create_patient():
+    # Arrange
     from database import create_patient
     input_line = "Ann Ables,1553,30"
-    expected = ["Ables", "Ann", 1553, 30, []]
+    expected = {"Last Name": "Ables",
+                "First Name": "Ann",
+                "MRN": 1553,
+                "Age": 30,
+                "Tests": []}
+    # Act
     answer = create_patient(input_line)
+    # Assert
     assert answer == expected
 
 
 def test_create_database():
+    # Arrange
     from database import create_database, db
-    # Arrange   
     db.clear()
     input_data = ["Ann Ables,1553,30",
                   "Bob Boyles,1,20"]
@@ -19,22 +23,31 @@ def test_create_database():
     create_database(input_data)
     # Assert
     assert len(db) == 2
-        
-    
+
 
 def test_create_database_other():
+    # Arrange
     from database import create_database, db
     db.clear()
     input_data = ["Ann Ables,1553,30",
                   ]
+    # Act
     create_database(input_data)
+    # Assert
     assert len(db) == 1
-    
-    
+
+
 def test_find_patient():
+    # Arrange
     from database import find_patient, db, create_database
     db.clear()
-    db.append(["Last", "First", 1, 30,[]])
+    new_patient = {"Last Name": "Last",
+                   "First Name": "First",
+                   "MRN": 1,
+                   "Age": 30,
+                   "Tests": []}
+    db.append(new_patient)
+    # Act
     answer = find_patient(1)
-    assert answer[0] == "Last"
-    
+    # Assert
+    assert answer == new_patient
