2a4522a314c2d38998a0aaa6711731119d8b9571
David Ward <david.a.ward@duke.edu>
2025-02-11 22:40:13 -0500

    Add module needed for error testing class

---
+++ my_math_calculator.py
@@ -0,0 +1,16 @@
+def sqrt(n):
+    
+    # if type(n) not in [int, float]:
+    #     raise TypeError("Parameter must be an int or float.")
+    # if n < 0:
+    #     raise ValueError("Parameter cannot be a negative number.")
+
+    x = n  # Input is initial approximation.  Could be improved
+    y = 1
+    e = 0.000001  # accuracy level
+
+    while (x-y > e):
+        x = (x + y) / 2
+        y = n / x
+        
+    return x
